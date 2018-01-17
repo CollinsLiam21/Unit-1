132bf0b9fff7d7cd168df42ca4e267926919e1c0
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-17 08:23:16 -0500

    Updated from Brython Server: 1/17/2018 8:23:16 AM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Liam Collins
+#1/17/18
+#face.py - Create face
 
+print('     xxxxx     ')
+print('    x     x    ')
+print(' ((   O O   )) ')
+print('    |  V  |    ')
+print('    | === |    ')
+print('     -----     ')
