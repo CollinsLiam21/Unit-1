377c897623712de9ce7353858e07668712fe6d50
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-17 08:29:31 -0500

    Updated from Brython Server: 1/17/2018 8:29:31 AM

--- km2miles.py
+++ km2miles.py
@@ -2,6 +2,6 @@
 #1/17/18
 #km2miles.py - km to miles
 
-kilo = int(input('number of kilometers'))
+kilo = int(input('number of kilometers '))
 miles = round(kilo/1.60934,3)
 print(kilo, 'Kilometers is', miles, 'Miles')
