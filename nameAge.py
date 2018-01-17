98f4bcc33d52ac101e534ecb53a016dbc13f5bbb
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-17 14:24:31 -0500

    Updated from Brython Server: 1/17/2018 2:24:31 PM

--- nameAge.py
+++ nameAge.py
@@ -2,10 +2,12 @@
 #1/17/18
 #nameAge.py
 
-name = input('Enter first and last name')
-age = input('Enter you age')
+name = input('Enter first and last name ')
+age = int(input('Enter you age '))
 
 name1, name2 = name.split()
 len(name1)
 len(name2)
-
+print('Your first name has', len(name1), 'letters in it')
+print('Your last name has', len(name2), 'letters in it')
+print('You will be', age+1, 'next year')
