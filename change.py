0a992af8df6c63773fe2f831e0cd7b2632809604
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-19 10:23:31 -0500

    Updated from Brython Server: 1/19/2018 10:23:30 AM

--- change.py
+++ change.py
@@ -1,1 +1,14 @@
+#Liam Collins
+#1/19/18
+#change.py
 
+totalCents = int(input('Enter number of cents: '))
+
+quarter = round(totalCents/25,0)
+print(quarter)
+
+nickel = round((totalCents-(quarter*25))/5,0)
+print(nickel)
+
+penny = round((totalCents-(quarter*25)-(nickel*5)),0)
+print(penny)
