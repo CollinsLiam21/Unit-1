d477f49e174d6aa5169a3918fd3b18eb0bd87605
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-22 08:48:10 -0500

    Updated from Brython Server: 1/22/2018 8:48:09 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,10 @@
+#Liam Collins
+#1/22/18
+#isItATriangle.py
 
+a = float(input('Side A: '))
+b = float(input('Side B: '))
+c = float(input('Side C: '))
+
+middleSide = a+b+c-max(a,b,c)-min(a,b,c)
+print(min(a,b,c)+middleSide>max(a,b,c))
