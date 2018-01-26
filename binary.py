6bcf0251679ac5128244a1f7643fbcf6ac4a2f1c
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-25 21:03:33 -0500

    Update binary.py

--- binary.py
+++ binary.py
@@ -12,4 +12,7 @@ fourth = (binary-eighth*10000000-seventh*1000000-sixth*100000-fifth*10000)//1000
 third = (binary-eighth*10000000-seventh*1000000-sixth*100000-fifth*10000-fourth*1000)//100
 second = (binary-eighth*10000000-seventh*1000000-sixth*100000-fifth*10000-fourth*1000-third*100)//10
 first = (binary-eighth*10000000-seventh*1000000-sixth*100000-fifth*10000-fourth*1000-third*100-second*10)//1
-print(eighth, seventh, sixth, fifth, fourth, third, second, first)
+
+decimalNotation = eighth*2**7+seventh*2**6+sixth*2**5+fifth*2**4+fourth*2**3+third*2**2+second*2**1+first*2**0
+print(decimalNotation)
+print(bin(decimalNotation))
